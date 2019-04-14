4d066de18840ba9b352a0cd88dc87c60b68b197c
Ben S <bmsr56@mst.edu>
2019-04-13 19:54:33 -0500

    Re-adding

---
+++ Joueur.py/games/stardash/tbow_classes.py
@@ -0,0 +1,45 @@
+# TheBirdsOfWar
+# Classes
+
+class Corvette:
+    """Shoots Lasers that can attack missiles.
+    """
+    def __init__(self):
+        pass
+
+
+class Martyr:
+    """Projects a shield that absorbs only lasers for protecting allies.
+    Shield regens at a rate of Corvette Damage (what does this mean?) 
+    Doesn't protect from missiles.
+    """
+
+    def __init__(self):
+        pass
+
+
+class Missileboat:
+    """Shoots Missiles that instantly kill ANY unit.
+    Can be shot down by Corvette in 2 turns.
+    Can be shot down by Missileboat in 1 turn.
+    """
+
+    def __init__(self):
+        pass
+
+class Miner:
+    """Mines minerals at a rate of 10
+    Can hold maximum of 20 minerals.
+    """
+
+    def __init__(self):
+        pass
+
+class Transport:
+    """Can hold 100 minerals
+    Has low health compared to other vessels.
+    """
+
+    def __init__(self):
+        pass
+
---
+++ Joueur.py/games/stardash/tbow_notes.md
@@ -0,0 +1,1 @@
+# tbow_notes
